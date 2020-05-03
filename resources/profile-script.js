02045b683d57d83aed0837959d0eb2672487f613
clandestinz <clandestine8945135@gmail.com>
2020-05-03 14:51:14 +0300

    -Added a new file structure -Remade photo organization -Added a mario77550 entry

--- resources/profile-script.js
+++ resources/profile-script.js
@@ -4,10 +4,10 @@ var navbar = document.getElementById("nav");
 var sticky = navbar.offsetTop;
 
 
-function addPhoto(photo){
+function addPhoto(photo,username){
   const div = document.createElement('div');
   div.className = "photocont";
-  div.innerHTML = '<div class="photocont"><img src="resources/profileres/' + photos[photo] + '.jpg"></div>';
+  div.innerHTML = '<img class="photo" src="resources/profileres/' + username + "/" + photos[photo] + '.jpg">';
   document.getElementById('photos').appendChild(div);
 }
 
@@ -15,18 +15,18 @@ function insertText(text, element){
   document.getElementById(element).innerHTML = text;
 }
 
-function addProfilePic(photo){
-  document.getElementById("profilepic").src = "resources/profileres/" + photo + ".jpg"
+function addProfilePic(photo, username){
+  document.getElementById("profilepic").src = "resources/profileres/"+ username+ "/" + photo + ".jpg"
 }
 
 function assembleHTML(){
-  addProfilePic(profileInfo.picture)
+  addProfilePic(profileInfo.picture, profileInfo.username)
   insertText(profileInfo.name, "name")
   insertText(profileInfo.username, "username")
   insertText(profileInfo.followers, "followers")
   insertText(profileInfo.following, "following")
   for(var i = 0; i < photos.length; i++){
-      addPhoto(i);
+      addPhoto(i, profileInfo.username);
   };
 }
 
