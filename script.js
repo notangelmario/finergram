98f770c67c3913aed4003f09b0517a72326b8819
clandestinz <clandestine8945135@gmail.com>
2020-05-04 14:05:25 +0300

    -Added a permanent cookie that stores theme option

--- script.js
+++ script.js
@@ -2,9 +2,33 @@ function insertVer(text){
   document.getElementById("version").innerHTML = text;
 }
 
+function setCookie(name,value,days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+function eraseCookie(name) {   
+  document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
+
 function dark_mode_flip(){
-  if (sessionStorage.getItem("theme") === "dark"){
-    sessionStorage.setItem("theme", "light");
+  if (getCookie("theme") == "dark"){
+    setCookie("theme", "light");
     [].forEach.call(document.getElementsByClassName("themed"), function(v,i,a){
       v.classList.add("light_mode");
       v.classList.remove("dark_mode");
@@ -14,7 +38,7 @@ function dark_mode_flip(){
     })
     console.log("set to light");
   } else {
-    sessionStorage.setItem("theme", "dark");
+    setCookie("theme", "dark");
     [].forEach.call(document.getElementsByClassName("themed"), function(v,i,a){
       v.classList.add("dark_mode");
       v.classList.remove("light_mode");
@@ -25,11 +49,15 @@ function dark_mode_flip(){
     console.log("set to dark");
   }
 }
-if (sessionStorage.getItem("theme") === "dark") {
-  sessionStorage.setItem("theme", "light");
+
+
+
+if (getCookie("theme") == "dark") {
+  setCookie("theme", "light");
   dark_mode_flip();
 } else {
-  sessionStorage.setItem("theme", "dark");
+  setCookie("theme", "dark");
   dark_mode_flip();
 };
+
 insertVer(0.5)
